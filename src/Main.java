5d1164c6d610ff2f043d26b4a687303a06348791
UgurKADIOGLU <ugrkadioglu@gmail.com>
2022-09-27 15:12:32 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+
+    public static void main(String[] args) {
+	sehirVer();
+    topla(5);
+    }
+
+    public static void ekle(){
+
+    }
+
+    public static void sil(){
+
+    }
+
+    public static void guncelle(){
+
+    }
+
+    public static int topla(int sayi){
+        return sayi;
+    }
+
+    public static String sehirVer(){
+        return "sehir";
+    }
+}
