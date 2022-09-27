0e9d2f30ad06e777b2db8cef1c177068fda29feb
UgurKADIOGLU <ugrkadioglu@gmail.com>
2022-09-27 15:18:37 +0300

    a

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,37 @@
 public class Main {
 
     public static void main(String[] args) {
-	sehirVer();
-    topla(5);
+        sehirVer();
+        topla(5);
+        topla2(2, 3, 4, 5, 6);
     }
 
-    public static void ekle(){
-
-    }
-
-    public static void sil(){
+    public static void ekle() {
 
     }
 
-    public static void guncelle(){
+    public static void sil() {
 
     }
 
-    public static int topla(int sayi){
+    public static void guncelle() {
+
+    }
+
+    public static int topla(int sayi) {
         return sayi;
     }
 
-    public static String sehirVer(){
+    public static String sehirVer() {
         return "sehir";
     }
+
+    public static int topla2(int... sayi) {
+        for (int s : sayi
+        ) {
+            System.out.println(s);
+        }
+        return 0;
+    }
 }
+
